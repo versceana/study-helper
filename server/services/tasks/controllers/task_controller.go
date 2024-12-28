154cc318d39f9d6566494b5417e10a85fcbfffee
d.yakupova <d.yakupova@innopolis.university>
2024-12-28 23:59:35 +0500

    Add changes

--- server/services/tasks/controllers/task_controller.go
+++ server/services/tasks/controllers/task_controller.go
@@ -1,1 +1,58 @@
 package controllers
+
+import (
+	"net/http"
+	"strconv"
+	"server/services/tasks/models"
+	"server/services/tasks/repository"
+	"github.com/gin-gonic/gin"
+)
+
+type TaskController struct {
+	Repo *repository.TaskRepository
+}
+
+func (ctrl *TaskController) GetTasks(c *gin.Context) {
+	tasks, err := ctrl.Repo.GetAllTasks()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, tasks)
+}
+
+func (ctrl *TaskController) AddTasks(c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if ctrl.Repo == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "repository not initialized"})
+		return
+	}
+
+	if err := ctrl.Repo.CreateTask(&task); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func (ctrl *TaskController) DeleteTask(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := ctrl.Repo.DeleteTask(uint(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
--- server/services/tasks/repository/task_repository.go
+++ server/services/tasks/repository/task_repository.go
@@ -1,1 +1,24 @@
 package repository
+
+import (
+	"gorm.io/gorm"
+	"server/services/tasks/models"
+)
+
+type TaskRepository struct {
+	DB *gorm.DB
+}
+
+func (repo *TaskRepository) GetAllTasks() ([]models.Task, error) {
+	var tasks [] models.Task
+	err := repo.DB.Find(&tasks).Error
+	return tasks, err
+}
+
+func (repo *TaskRepository) CreateTask(task *models.Task) error {
+	return repo.DB.Create(task).Error
+}
+
+func (repo *TaskRepository) DeleteTask(id uint) error {
+	return repo.DB.Delete(&models.Task{}, id).Error
+}
